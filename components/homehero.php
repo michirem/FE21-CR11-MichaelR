1ce6e4cf4f710fc0ad3951fd6f72f0951a827c44
michirem <dariuss@gmx.at>
2021-04-30 19:06:41 +0200

    corrected background image link for hero section

--- components/homehero.php
+++ components/homehero.php
@@ -1,3 +1,4 @@
-<div class="d-flex justify-content-center align-items-center" style="background-image: url('../pictures/homehero.png'); height: 50vh; background-size: cover; background-repeat: no-repeat; background-position: 50% 30%;">
-    <h1 class="text-center text-light">Such Pet,<br><br><br>Much Wow!</h1>
+<div class="d-flex justify-content-evenly align-items-center" style="background-image: url(pictures/homehero.png); height: 50vh; background-size: cover; background-repeat: no-repeat; background-position: 50% 50%;">
+    <h1 class="text-center">Such Pet</h1>
+    <h1 class="text-center">Much Wow!</h1>
 </div>
