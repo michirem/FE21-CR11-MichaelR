654a36cc8bef718bfca0952b49b3513e36f4794a
michirem <dariuss@gmx.at>
2021-05-01 13:39:23 +0200

    added "add pets" button with link to create.php

--- components/homemain.php
+++ components/homemain.php
@@ -8,6 +8,10 @@ $result = mysqli_query($connect, $query);
 for ($set = array(); $row = mysqli_fetch_assoc($result); $set[] = $row);
 
     if(mysqli_num_rows($result) > 0 && isset($_SESSION['adm'])) {
+        echo
+        '<div class="d-flex justify-content-center my-3">
+            <a href= "create.php" class="btn btn-primary">Add Pets</a>
+        </div>';
         foreach($set as $value){
             echo showPetAdmin($value['picture'], $value['name'], $value['description'], $value['age'], $value['address'], $value['city'], $value['zip'], $value['animal_id']);
             }
