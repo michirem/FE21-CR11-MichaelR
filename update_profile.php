5d39fdf3761accf65d22a03d3ae5c6dfe823cbdf
michirem <dariuss@gmx.at>
2021-05-01 12:46:42 +0200

    completed profile update form with input sanitization

--- update_profile.php
+++ update_profile.php
@@ -3,6 +3,7 @@ session_start();
 
 require_once 'actions/db_connect.php';
 require_once 'actions/file_upload.php';
+require_once 'components/functions.php';
 
 // if session is not set this will redirect to login page
 if( !isset($_SESSION['adm']) && !isset($_SESSION['user']) ) {
@@ -28,15 +29,48 @@ if (isset($_GET['id'])) {
 
 //update
 $class = 'd-none';
+$error = false;
+$f_name = $l_name = $email = $picture = '';
+$fnameError = $lnameError = $emailError = $picError = '';
+
 if (isset($_POST["submit"])) {
-    $f_name = $_POST['first_name'];
-    $l_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $id = $_POST['id'];
+    $f_name = sanitize($_POST['first_name']);
+    $l_name = sanitize($_POST['last_name']);
+    $email = sanitize($_POST['email']);
+    $id = sanitize($_POST['id']);
+
+    // basic name validation
+    if (empty($f_name) || empty($l_name)) {
+        $error = true;
+        $fnameError = "Please enter your full name and surname";
+    } else if (strlen($f_name) < 3 || strlen($l_name) < 3) {
+        $error = true;
+        $fnameError = "Name and surname must have at least 3 characters.";
+    } else if (!preg_match("/^[a-zA-Z]+$/", $f_name) || !preg_match("/^[a-zA-Z]+$/", $l_name)) {
+        $error = true;
+        $fnameError = "Name and surname must contain only letters and no spaces.";
+    }
+   
+    //basic email validation
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = true;
+        $emailError = "Please enter valid email address.";
+    } else {
+    // checks whether the email exists or not
+        $query = "SELECT email FROM user WHERE email='$email'";
+        $result = mysqli_query($connect, $query);
+        $count = mysqli_num_rows($result);
+        if ($count != 0) {
+            $error = true;
+            $emailError = "Provided Email is already in use.";
+        }
+    }
+
     //variable for upload pictures errors is initialized
     $uploadError = '';    
     $pictureArray = file_upload($_FILES['picture'], 'user'); //file_upload() called
     $picture = $pictureArray->fileName;
+
     if ($pictureArray->error === 0) {       
         ($_POST["picture"] == "avatar.webp") ?: unlink("pictures/{$_POST["picture"]}");
         $sql = "UPDATE user SET first_name = '$f_name', last_name = '$l_name', email = '$email', picture = '$pictureArray->fileName' WHERE user_id = {$id}";
@@ -91,22 +125,30 @@ $connect->close();
                 <table class="table">
                     <tr>
                         <th>First Name</th>
-                        <td><input class="form-control" type="text"  name="first_name" placeholder ="First Name" value="<?php echo $f_name ?>"  /></td>
+                        <td><input class="form-control" type="text"  name="first_name" placeholder ="First Name" value="<?php echo $f_name ?>"/>
+                        <span class="text-danger"> <?php echo $fnameError; ?> </span>
+                        </td>
                     </tr>
                     <tr>
                         <th>Last Name</th>
-                        <td><input class="form-control" type= "text" name="last_name"  placeholder="Last Name" value ="<?php echo $l_name ?>" /></td>
+                        <td><input class="form-control" type= "text" name="last_name"  placeholder="Last Name" value ="<?php echo $l_name ?>" />
+                        <span class="text-danger"> <?php echo $fnameError; ?> </span>
+                        </td>
                     </tr>
                     <tr>
                         <th>Email</th>
-                        <td><input class="form-control" type="email" name="email" placeholder= "Email" value= "<?php echo $email ?>" /></td>
+                        <td><input class="form-control" type="email" name="email" placeholder= "Email" value= "<?php echo $email ?>" />
+                        <span  class="text-danger"> <?php echo $emailError; ?> </span>
+                        </td>
                     </tr>
                     <tr>
                         <th>Picture</th>
-                        <td><input class="form-control" type="file" name="picture" /></td>
+                        <td><input class="form-control" type="file" name="picture" />
+                        <span class="text-danger"> <?php echo $picError; ?> </span>
+                        </td>
                     </tr>
                     <tr>
-                        <input type= "hidden" name= "id" value= "<?php echo $data['id'] ?>" />
+                        <input type= "hidden" name= "id" value= "<?php echo $data['user_id'] ?>" />
                         <input type= "hidden" name= "picture" value= "<?php echo $picture ?>" />
                         <td> </td>
                         <td><button name="submit" class="btn btn-primary mx-2" type= "submit">Save Changes</button><a href= "profile.php"><button class="btn btn-danger mx-2" type="button">Cancel</button></a></td>
